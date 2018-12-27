9970ec5afe0ea759689834aa51c516818144a888
Rey Abolofia <rabolofia@newrelic.com>
2018-12-27 11:34:44 -0800

    Test distributed tracing.

--- _integrations/nrlambda/handler_test.go
+++ _integrations/nrlambda/handler_test.go
@@ -28,6 +28,14 @@ func testApp(cfgfn func(*newrelic.Config), t *testing.T) newrelic.Application {
 	return app
 }
 
+func distributedTracingEnabled(config *newrelic.Config) {
+	config.CrossApplicationTracer.Enabled = false
+	config.DistributedTracer.Enabled = true
+	config.ServerlessMode.AccountID = "1"
+	config.ServerlessMode.TrustKey = "1"
+	config.ServerlessMode.PrimaryAppID = "1"
+}
+
 func TestColdStart(t *testing.T) {
 	originalHandler := func(c context.Context) {}
 	app := testApp(nil, t)
@@ -135,6 +143,69 @@ func TestSetWebRequest(t *testing.T) {
 		Intrinsics: map[string]interface{}{
 			"name":             "WebTransaction/Go/functionName",
 			"nr.apdexPerfZone": "S",
+		},
+		UserAttributes: map[string]interface{}{},
+		AgentAttributes: map[string]interface{}{
+			"aws.lambda.coldStart": true,
+		},
+	}})
+}
+
+func makePayload(app newrelic.Application) string {
+	txn := app.StartTransaction("hello", nil, nil)
+	return txn.CreateDistributedTracePayload().Text()
+}
+
+func TestDistributedTracing(t *testing.T) {
+	originalHandler := func(events.APIGatewayProxyRequest) {}
+	app := testApp(distributedTracingEnabled, t)
+	wrapped := Wrap(originalHandler, app)
+	w := wrapped.(*wrappedHandler)
+	w.functionName = "functionName"
+
+	req := events.APIGatewayProxyRequest{
+		Headers: map[string]string{
+			"X-Forwarded-Port":                     "4000",
+			"X-Forwarded-Proto":                    "HTTPS",
+			newrelic.DistributedTracePayloadHeader: makePayload(app),
+		},
+	}
+	reqbytes, err := json.Marshal(req)
+	if err != nil {
+		t.Error("unable to marshal json", err)
+	}
+
+	resp, err := wrapped.Invoke(context.Background(), reqbytes)
+	if err != nil {
+		t.Error(err, string(resp))
+	}
+	app.(internal.Expect).ExpectMetrics(t, []internal.WantMetric{
+		{Name: "Apdex", Scope: "", Forced: true, Data: nil},
+		{Name: "Apdex/Go/functionName", Scope: "", Forced: false, Data: nil},
+		{Name: "DurationByCaller/App/1/1/HTTPS/all", Scope: "", Forced: false, Data: nil},
+		{Name: "DurationByCaller/App/1/1/HTTPS/allWeb", Scope: "", Forced: false, Data: nil},
+		{Name: "HttpDispatcher", Scope: "", Forced: true, Data: nil},
+		{Name: "Supportability/DistributedTrace/AcceptPayload/Success", Scope: "", Forced: true, Data: nil},
+		{Name: "TransportDuration/App/1/1/HTTPS/all", Scope: "", Forced: false, Data: nil},
+		{Name: "TransportDuration/App/1/1/HTTPS/allWeb", Scope: "", Forced: false, Data: nil},
+		{Name: "WebTransaction", Scope: "", Forced: true, Data: nil},
+		{Name: "WebTransaction/Go/functionName", Scope: "", Forced: true, Data: nil},
+	})
+	app.(internal.Expect).ExpectTxnEvents(t, []internal.WantEvent{{
+		Intrinsics: map[string]interface{}{
+			"name":                     "WebTransaction/Go/functionName",
+			"nr.apdexPerfZone":         "S",
+			"parent.account":           "1",
+			"parent.app":               "1",
+			"parent.transportType":     "HTTPS",
+			"parent.type":              "App",
+			"guid":                     internal.MatchAnything,
+			"parent.transportDuration": internal.MatchAnything,
+			"parentId":                 internal.MatchAnything,
+			"parentSpanId":             internal.MatchAnything,
+			"priority":                 internal.MatchAnything,
+			"sampled":                  internal.MatchAnything,
+			"traceId":                  internal.MatchAnything,
 		},
 		UserAttributes: map[string]interface{}{},
 		AgentAttributes: map[string]interface{}{
