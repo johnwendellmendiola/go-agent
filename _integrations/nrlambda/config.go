2f4cfb310c22a510a800a65aea79755e0946b1ba
will@newrelic.com <will@newrelic.com>
2019-01-02 16:38:22 -0800

    Add nrlambda.NewConfig test

--- _integrations/nrlambda/config.go
+++ _integrations/nrlambda/config.go
@@ -10,16 +10,20 @@ import (
 // NewConfig populates a newrelic.Config with correct default settings for a
 // Lambda serverless environment.
 func NewConfig() newrelic.Config {
+	return newConfigInternal(os.Getenv)
+}
+
+func newConfigInternal(getenv func(string) string) newrelic.Config {
 	cfg := newrelic.NewConfig("", "")
 
 	cfg.ServerlessMode.Enabled = true
 
-	cfg.ServerlessMode.AccountID = os.Getenv("NEW_RELIC_ACCOUNT_ID")
-	cfg.ServerlessMode.TrustedAccountKey = os.Getenv("NEW_RELIC_TRUST_KEY")
-	cfg.ServerlessMode.PrimaryAppID = os.Getenv("NEW_RELIC_PRIMARY_APPLICATION_ID")
+	cfg.ServerlessMode.AccountID = getenv("NEW_RELIC_ACCOUNT_ID")
+	cfg.ServerlessMode.TrustedAccountKey = getenv("NEW_RELIC_TRUST_KEY")
+	cfg.ServerlessMode.PrimaryAppID = getenv("NEW_RELIC_PRIMARY_APPLICATION_ID")
 
-	if s := os.Getenv("NEW_RELIC_APDEX_T"); "" != s {
-		if apdex, err := time.ParseDuration(s + "s"); nil != err {
+	if s := getenv("NEW_RELIC_APDEX_T"); "" != s {
+		if apdex, err := time.ParseDuration(s + "s"); nil == err {
 			cfg.ServerlessMode.ApdexThreshold = apdex
 		}
 	}
